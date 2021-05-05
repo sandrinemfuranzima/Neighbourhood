5a3ff33bf5f25e8ad6a25f86ca3ef5a0ea675ebb
sandrinemfuranzima <75666150+sandrinemfuranzima@users.noreply.github.com>
2021-05-05 12:26:27 +0200

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Neighbourhood,UserProfile,Business, Posts,Comment
 
+
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(UserProfile)
