6ac05064c89783d2bbeeff52a734a3cbdd2c2072
sandrinemfuranzima <75666150+sandrinemfuranzima@users.noreply.github.com>
2021-05-05 12:28:17 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Neighbourhood, Posts, Comment, Business, UserProfile
 
+
 class HoodForm(forms.ModelForm):
     class Meta: 
         model = Neighbourhood
